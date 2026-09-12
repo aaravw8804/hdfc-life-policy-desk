2c08f043bc597f2dd845946f08b9f27ce4aa1e31
Aarav <aaravpw04@gmail.com>
2026-09-12 22:38:02 +0530

    remove unnecessary configuration and class files; update .gitignore to include new patterns

--- target/classes/db/migration/V2__seed_reference_riders.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO riders (code, name) VALUES ('ACCIDENT_COVER', 'Accident Cover');
-INSERT INTO riders (code, name) VALUES ('CRITICAL_ILLNESS', 'Critical Illness');
-INSERT INTO riders (code, name) VALUES ('WAIVER_OF_PREMIUM', 'Waiver of Premium');
